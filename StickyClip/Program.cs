43856557d510c2a5a710f9aed5a64d3998d62e08
gimdh <36066149+gimdh@users.noreply.github.com>
2020-08-27 17:11:20 +0900

    Prevent multiple instances

--- StickyClip/Program.cs
+++ StickyClip/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace StickyClip
 {
@@ -8,13 +10,28 @@ namespace StickyClip
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            using (var stickyNoteManager = new StickyNoteManager())
+            using (Mutex mutex = new Mutex(false, GetGUID()))
             {
-                Application.Run(new TrayAppContext());
+                if (!mutex.WaitOne(0, false))
+                {
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                using (var stickyNoteManager = new StickyNoteManager())
+                {
+                    Application.Run(new TrayAppContext());
+                }
             }
+
+        }
+
+        private static string GetGUID()
+        {
+            return ((GuidAttribute)typeof(Program).Assembly
+                .GetCustomAttributes(typeof(GuidAttribute), true).GetValue(0)).Value;
         }
     }
 }
